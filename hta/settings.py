369bd50aeb34417cd857abd6213bfd744dbc5c34
Antony <antomutwiri2001@gmail.com>
2025-11-05 09:38:02 +0300

    media root urls

--- hta/settings.py
+++ hta/settings.py
@@ -265,7 +265,8 @@ AUTH_USER_MODEL = 'users.CustomUser'
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://bptap.health.go.ke/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
